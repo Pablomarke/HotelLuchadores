53059483c8656208fef690c9c7bb56b02a852233
Pablo Márquez <pablomarquezartesano@gmail.com>
2023-06-29 18:17:16 +0200

    Creado struct de reservation

--- HotelLuchadores.playground/Contents.swift
+++ HotelLuchadores.playground/Contents.swift
@@ -1,13 +1,40 @@
 import UIKit
+import Foundation
 
-
+// Clientes
 struct Client {
     let name: String
     var Age: Int
     var height: Float
 }
-
+///creación de clientes
 var goku = Client(name: "Goku", Age: 40, height: 1.75)
 var vegetta = Client(name: "Vegeta", Age: 46, height: 1.64)
+var bulma = Client(name: "Bulma", Age: 28, height: 1.65)
 
-print(goku.Age)
+
+// Reserva
+struct Reservation {
+    
+    //let id: String = "pagoku"
+    let hotelName : String = "Gran Hotel de Isla Papaya"
+    var clientList: [Client]
+    var duration: Int
+    var price: Int = 50
+    var breakfast: Bool
+    
+    func priceReservation() -> Int {
+        var price = self.price
+        var clients = self.clientList.count
+        if breakfast == true {
+            price += 15
+        }
+        var total = price * clients * duration
+        return total
+        
+    }
+}
+
+var r2d2 = Reservation(clientList: [goku, bulma], duration: 2, breakfast: false)
+r2d2.priceReservation()
+
