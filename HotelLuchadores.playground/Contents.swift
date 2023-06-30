b6d480b7233a8f234b456172005f4976bf4cf136
Pablo Márquez <pablomarquezartesano@gmail.com>
2023-06-30 10:50:15 +0200

    Implementado parcialmente hotelReservation Manager. Cambiado a class el struct reservation

--- HotelLuchadores.playground/Contents.swift
+++ HotelLuchadores.playground/Contents.swift
@@ -5,7 +5,7 @@ import Foundation
 struct Client {
     let name: String
     var Age: Int
-    var height: Float
+    var height: Double
 }
 ///creación de clientes
 var goku = Client(name: "Goku", Age: 40, height: 1.75)
@@ -14,27 +14,64 @@ var bulma = Client(name: "Bulma", Age: 28, height: 1.65)
 
 
 // Reserva
-struct Reservation {
+class Reservation {
     
-    //let id: String = "pagoku"
+    var id: String = "GHIP"
     let hotelName : String = "Gran Hotel de Isla Papaya"
-    var clientList: [Client]
-    var duration: Int
+    var clientList: [Client] = []
+    var duration: Int = 0
     var price: Int = 50
     var breakfast: Bool
     
+    init( clientList: [Client], duration:Int, breakfast: Bool) {
+        //self.id = id
+        //self.hotelName = hotelName
+        self.duration = duration
+        //self.price = price
+        self.breakfast = breakfast
+    }
+    
     func priceReservation() -> Int {
         var price = self.price
-        var clients = self.clientList.count
+        let clients = self.clientList.count
         if breakfast == true {
             price += 15
         }
-        var total = price * clients * duration
+        let total = price * clients * duration
         return total
-        
     }
 }
 
+/// Creación de reservas
 var r2d2 = Reservation(clientList: [goku, bulma], duration: 2, breakfast: false)
 r2d2.priceReservation()
+
+
+// Errores en la reserva
+
+enum reservationError {
+    case id
+    case repeatClient
+    case noReservation
+}
+
+// Gestion de reservas del hotel
+
+class HotelReservationManager: Reservation {
+    var reservationList: [Reservation] = []
+    var reservation : Reservation
+    
+    init( reservation: Reservation) {
+        self.reservation = reservation
+        super.init(clientList: reservation.clientList, duration: reservation.duration, breakfast: reservation.breakfast)
+    }
+    
+    func makeReservation(reservation: Reservation) {
+        self.reservationList.append(reservation)
+       
+    }
+}
+
+let res = HotelReservationManager(reservation: r2d2)
+
 
